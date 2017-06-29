3ea968ffc050edabfb48aa4f8b4152211ebc1289
Hui Zhou <zhou_hui@outlook.com>
2017-06-29 08:02:10 -0700

    Update doc and remove one log line

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Change Log
 
+## 0.1.0
+- Add icon to diff the specified commit
+
 ## 0.0.1
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -8,11 +8,6 @@ import { CommandCenter } from './commands';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "scm" is now active!');
-
     let model = new Model();
     let commandCenter = new CommandCenter(model);
     context.subscriptions.push(commandCenter, model);
