b32a6c3a46db0d337eb248988248f73811038fcd
Hui Zhou <dazhou@microsoft.com>
2018-10-16 12:13:02 -0700

    Add git blame

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,7 @@ import { CommandCenter } from './commands';
 import { HistoryViewProvider } from './historyViewProvider';
 import { ExplorerViewProvider } from './explorerViewProvider';
 import { InfoViewProvider } from './infoViewProvider';
+import { BlameViewProvider } from './blameViewProvider';
 
 export function activate(context: ExtensionContext) {
     let gitService = new GitService();
@@ -14,9 +15,10 @@ export function activate(context: ExtensionContext) {
     let historyViewProvider = new HistoryViewProvider(model, gitService);
     let infoViewProvider = new InfoViewProvider(model, gitService);
     let explorerProvider = new ExplorerViewProvider(model, gitService);
+    let blameViewProvider = new BlameViewProvider(model, gitService);
     let commandCenter = new CommandCenter(model, gitService, historyViewProvider, infoViewProvider);
 
-    context.subscriptions.push(gitService, model, historyViewProvider, infoViewProvider, explorerProvider, commandCenter);
+    context.subscriptions.push(gitService, model, historyViewProvider, infoViewProvider, explorerProvider, commandCenter, blameViewProvider);
 }
 
 export function deactivate() {
